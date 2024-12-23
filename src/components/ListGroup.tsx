7a2b456ede00450b3f80591238e5e01b46c659b3
mb2753 <mb2753@cornell.edu>
2024-12-23 14:46:44 -0500

    List item component

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,5 +1,24 @@
+
 function ListGroup(){
-    return <h1>List Group</h1>
+    const items = [
+        'New York',
+        'San Francisco',
+        'Tokyo',
+        'London',
+        'Paris'
+    ]
+    return (
+    <>
+        <h1>List</h1>
+      
+        {items.length === 0 && <p>No item found</p>}
+        <ul className="list-group">
+            {items.map((item, index) => (
+            <li className = "list-group-item" key={item} onClick={() => console.log(item, index)}>{item}</li>
+            ))} 
+          </ul>
+    </>
+  );
 }
 
 export default ListGroup
